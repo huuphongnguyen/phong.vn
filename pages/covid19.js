941083afca83cbd37c71795c77249c20c8d416d3
PHONG <huuphong.nguyen1507@gmail.com>
2021-08-12 14:41:10 +0000

    Update new split page /covid19

---
+++ pages/covid19.js
@@ -0,0 +1,40 @@
+import Covid19Section from "../components/structures/Covid19Section";
+
+export async function getStaticProps() {
+  const apiUrlCovid19Global = process.env.COVID_19_GLOBAL_API_URL;
+  const apiOptionCovid19Global = { method: "GET" };
+  const apiDataCovid19Global = await fetch(
+    apiUrlCovid19Global,
+    apiOptionCovid19Global
+  );
+  const dataFromCovid19GlobalAPI = await apiDataCovid19Global.json();
+
+  const apiUrlCovid19Vietnam = process.env.COVID_19_VIETNAM_API_URL;
+  const apiOptionCovid19Vietnam = { method: "GET" };
+  const apiDataCovid19Vietnam = await fetch(
+    apiUrlCovid19Vietnam,
+    apiOptionCovid19Vietnam
+  );
+  const dataFromCovid19VietnamAPI = await apiDataCovid19Vietnam.json();
+
+  return {
+    props: {
+      resultsCovid19Global: dataFromCovid19GlobalAPI,
+      resultsCovid19Vietnam: dataFromCovid19VietnamAPI,
+    },
+  };
+}
+
+export default function Covid19({
+  resultsCovid19Global,
+  resultsCovid19Vietnam,
+}) {
+  return (
+    <div className="w-full flex items-center justify-center mt-12 md:mt-[200px]">
+      <Covid19Section
+        dataGlobal={resultsCovid19Global}
+        dataVietnam={resultsCovid19Vietnam}
+      />
+    </div>
+  );
+}
