572f1802f4c7046fa053cbaac455fbcca3c3e1e3
PHONG <huuphong.nguyen1507@gmail.com>
2021-08-16 07:21:50 +0000

    Update

--- pages/inspiration/reddit-wallpaper.js
+++ pages/inspiration/reddit-wallpaper.js
@@ -13,22 +13,23 @@ export async function getStaticProps() {
   const res = await axios.get(axiosUrl);
   const data = await res.data;
   var dataFromRedditWallpaperAPI = [];
-  data.data.children.map((item) => {
-    try {
-      const parent_img = item?.data.preview.images[0].resolutions[3].url;
-      dataFromRedditWallpaperAPI.push({
-        id: item.data.id,
-        title: item.data.title,
-        url: item.data.url,
-        author: item.data.author,
-        small_img: parent_img,
-        ups: item.data.ups,
-        permalink: item.data.permalink,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  });
+  data &&
+    data.data.children.map((item) => {
+      try {
+        const parent_img = item?.data.preview.images[0].resolutions[3].url;
+        dataFromRedditWallpaperAPI.push({
+          id: item.data.id,
+          title: item.data.title,
+          url: item.data.url,
+          author: item.data.author,
+          small_img: parent_img,
+          ups: item.data.ups,
+          permalink: item.data.permalink,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    });
 
   return {
     props: {
